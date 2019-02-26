d17b74cf3ba1f4d261af3937a0fab150173df215
Andres Gonzalez <andresg9108@gmail.com>
2019-02-26 06:12:36 -0600

    Mejorando Funciones JavaScript

--- src/js/main.js
+++ src/js/main.js
@@ -4,17 +4,19 @@ var g_sRouteTemplate = 'src/template/';
 
 $(function(){
 	let oData = {};
-	loadTemplate('header', 'header', '', oData)
-	loadTemplate('body', 'section', '', oData)
-	loadTemplate('footer', 'footer', '', oData)
+	loadTemplate('header', 'header', oData)
+	loadTemplate('body', 'section', oData)
+	loadTemplate('footer', 'footer', oData)
 });
 
 /*
 */
-function loadTemplate(sRouteTemplate, sTag, sClassCss, oData){
+function loadTemplate(sRouteTemplate, sTag, oData){
 	let sRoute = g_sRouteTemplate+sRouteTemplate+'.hbs';
 	let sTemplate = Hbs[sRoute](oData);
 	let isTemplate = $(sTag).attr('data-template');
+	isTemplate = (isTemplate == 'true');
+	let sClassCss = $(sTag).attr('data-styles');
 	if(isTemplate){
 		$(sTag).removeClass();
 		$(sTag).addClass(sClassCss);
